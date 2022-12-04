97e185d1045a557dcc869b0c636f9add86722f35
Niklas <niklas.wojtkowiak@real.edu.ee>
2022-12-03 21:13:49 -0500

    Current Progress

---
+++ SFSMod/Class1.cs
@@ -0,0 +1,98 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using SFS.Input;
+using SFS.IO;
+using SFS.Platform;
+using SFS.Translations;
+using SFS.UI;
+using SFS.UI.ModGUI;
+using UnityEngine;
+using System.Reflection;
+using UITools;
+using JetBrains.Annotations;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+using SFS.Parts;
+using System;
+using SFS.Variables;
+using Type = SFS.UI.ModGUI.Type;
+
+namespace SSCMod
+{
+    [HarmonyPatch(typeof(CreateWorldMenu), nameof(CreateWorldMenu.OpenSelectSolarSystemMenu))]
+    class AddCustomButton
+    {
+        public static GameObject holder;
+        public static bool snapping;
+        public static readonly int MainWindowID = Builder.GetRandomID();
+        static bool Prefix(CreateWorldMenu __instance)
+        {
+            Traverse traverse = Traverse.Create(__instance);
+
+            SizeSyncerBuilder.Carrier horizontal;
+            List<MenuElement> buttons = new List<MenuElement>
+            {
+                TextBuilder.CreateText().Text(() => Loc.main.Select_Solar_System),
+                ElementGenerator.VerticalSpace(30),
+                new SizeSyncerBuilder(out horizontal)
+            };
+            AddButton(Loc.main.Default_Solar_System, "");
+
+            if (DevSettings.FullVersion && FileLocations.SolarSystemsFolder.FolderExists())
+            {
+                foreach (FolderPath item in FileLocations.SolarSystemsFolder.GetFoldersInFolder(recursively: false))
+                {
+                    if (item.FolderName != "Example")
+                    {
+                        AddButton(Loc.main.Custom_Solar_System.Inject(item.FolderName, "name"), item.FolderName);
+                    }
+                }
+            }
+           
+
+            buttons.Add(ElementGenerator.VerticalSpace(60)); // Seperates the button from the rest of the solar systems.
+            buttons.Add(ButtonBuilder.CreateButton(horizontal, () => "Create Solar System", delegate
+            {
+                // Custom solar system creator button code is called here. Just using Debug.Log as an example.
+                // Tip: You can pass __instance to your custom solar system creator method if you want to set the new 
+                // world's solar system to the custom one using:
+                holder = Builder.CreateHolder(Builder.SceneToAttach.CurrentScene, "SCC GUI Holder");
+                Window window = Builder.CreateWindow(holder.transform, MainWindowID, 1600, 1500, 0, 750, true, true, 0.95f, "Solar System Creator");
+                window.CreateLayoutGroup(Type.Vertical);
+
+                Container inputContainer = Builder.CreateContainer(window);
+
+                InputWithLabel percentInput = Builder.CreateInputWithLabel(window, 380, 50, labelText: "something", inputText: "1");
+                inputContainer.CreateLayoutGroup(Type.Horizontal, spacing: 10f);;
+                Builder.CreateToggleWithLabel(window, 380, 50, () => !snapping, () => snapping ^= true, 0, 0,"Snap to Parts");
+
+
+
+
+                //SFS.UI.ModGUI.Window();
+                // Traverse traverse = Traverse.Create(__instance);
+                // traverse.Method("SetSolarSystem", solarSystemName);
+
+                Debug.Log("Time to open the solar system creator!");
+            }, CloseMode.Current));
+
+
+            MenuGenerator.OpenMenu(CancelButton.Cancel, CloseMode.Current, buttons.ToArray());
+
+            void AddButton(string text, string solarSystemName)
+            {
+                buttons.Add(ButtonBuilder.CreateButton(horizontal, () => text, delegate
+                {
+                    traverse.Method("SetSolarSystem", solarSystemName);
+                }, CloseMode.Current));
+            }
+
+            string name = "Creator";
+            Scene scene = SceneManager.CreateScene(name);
+            SceneManager.LoadScene(name);
+
+            // Skip orginal code, since it's already been run here.
+            return false;
+        }
+    }
+}
---
+++ SFSMod/MyMod.cs
@@ -0,0 +1,116 @@
+﻿
+using HarmonyLib;
+using ModLoader;
+using ModLoader.Helpers;
+using System.Collections.Generic;
+using UnityEngine;
+using UITools;
+using SFS.IO;
+using SFS.Variables;
+using System;
+using SFS.UI;
+
+namespace SFSMod
+{
+    /**
+     * You only need to implement the Mod class once in your mod. The Mod class is how 
+     * you tell the mod loader what it needs to load and execute.
+     */
+
+    /**
+    [Serializable]
+    public class Data
+    {
+        // Bool_Local and Float_Local are classes in SFS.Variables that let you detect onChange events.
+        public Bool_Local myBool = new Bool_Local();
+        public Float_Local myFloat = new Float_Local();
+    }
+    */
+
+    
+
+    public class MyMod : Mod
+    {
+        public static MyMod Main;
+
+        // this ModNameID can be whatever you want
+        public override string ModNameID => "SCC";
+
+        public override string DisplayName => "SolarSystem Creator";
+
+        public override string Author => "0xNim";
+
+        public override string MinimumGameVersionNecessary => "1.5.7";
+
+        // I recommend use MAJOR.MINOR.PATCH Semantic versioning. 
+        // Reference link: https://semver.org/ 
+        public override string ModVersion => "v0.0.1";
+
+        public override string Description => "A mod to create your own Solar System!";
+
+        // With this variable you can define if your mods needs the others mods to work
+        public override Dictionary<string, string> Dependencies
+        {
+            get
+            {
+                new Dictionary<string, string> { { "UITools", "1.1.1" } };
+                return this._dependencies;
+            }
+        }
+
+        // Here you can specify which mods and version you need
+        private Dictionary<string, string> _dependencies = new Dictionary<string, string>() {};
+
+        public static FolderPath modFolder;
+
+        public override void Early_Load()
+        {
+            // This is for a singleton pattern. you can see more about singleton here https://www.c-sharpcorner.com/UploadFile/8911c4/singleton-design-pattern-in-C-Sharp/
+            Main = this;
+
+            /*
+            ModFolder is an existing variable in the base class. It cannot be accessed by other classes by
+            default, so we copy it to the var.
+            */
+            modFolder = new FolderPath(ModFolder);
+
+            // Make use to use Debug.log from UnityEngine
+            Debug.Log("Running Early load code");
+
+            // Use early load to use Harmony and patch function
+            Harmony harmony = new Harmony(ModNameID);
+            // I use ModNameID in Harmony, because you need to pass string ID to create an instance of Harmony.
+
+            // This function finds all the patches you have created and runs them
+            harmony.PatchAll();
+
+            // you can subscribe to scene changes
+            SceneHelper.OnWorldSceneLoaded += this.OnWorld;
+            SceneHelper.OnBuildSceneLoaded += this.OnBuild;
+        }
+
+        public override void Load()
+        {
+            Debug.Log("Running Load code");
+
+            // init your keybindings
+            Settings.Setup();
+
+        }
+
+        // When the world scene is loaded
+        private void OnWorld()
+        {
+            Debug.Log("On World");
+        }
+
+        // When the Build scene is loaded
+        private void OnBuild()
+        {
+            Debug.Log("On Build");
+        }
+
+
+
+    }
+}
